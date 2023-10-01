115046b873e3a1078ee052751693bd0cd9bdc5d5
Sean Jackson <105176571+seanjackson6325@users.noreply.github.com>
2023-10-01 18:18:46 -0400

    Update rocket.py

--- rocket.py
+++ rocket.py
@@ -2,7 +2,13 @@
 """
 This is a program I made for a PHY224 lab at the University of Toronto.
 Given some data about the Saturn V rocket, I developed methods to
-accurately predict it's position 
+accurately predict it's position.
+
+I used a linear regression algorithm to find a linear function
+desribing the rocket's position, and I also used the curve_fit
+function provided by matplotlib. I compared the results of these
+two functions on the plot, and the result was that there is little
+to no difference between the two methods.
 """
 
 
